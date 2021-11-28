1dc757bec4d7fbb0406f2307873116dc3bfe6456
Satinder Singh <satinder_2000@outlook.com>
2021-11-28 21:52:45 +0000

    Project Initialised

---
+++ gdfNow-jpa/src/test/java/org/gdf/GdfNowJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdfNowJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
