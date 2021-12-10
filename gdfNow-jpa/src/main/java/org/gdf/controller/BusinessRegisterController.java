b713e68c2f68f55dff739f200d795fa0add74221
Satinder Singh <satinder_2000@outlook.com>
2021-12-10 23:54:42 +0000

    W.I.P

---
+++ gdfNow-jpa/src/main/java/org/gdf/controller/BusinessRegisterController.java
@@ -0,0 +1,11 @@
+package org.gdf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class BusinessRegisterController {
+	
+	//@Controller
+	//@RequestMapping(path="/businessregister")
+
+}
